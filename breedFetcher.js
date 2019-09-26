b0cc9d5ce488ecc84415ea756149ce55ca67d6ba
Aiman Azmie <aimanaaw@gmail.com>
2019-09-26 20:42:04 +0000

    Breed fetcher test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,19 @@
 const request = require('request');
-const userInputURL = process.argv.slice(2);
-const givenURL = 'https://api.thecatapi.com/v1/breeds/search?q=' + userInputURL;
 
-request.get(givenURL, (err, response, body) => {
-  if (err) {
-    console.log("Breed not found: ", err);
-  }
-  if (JSON.parse(body)[0] === undefined) {
-    console.log("Error: The breed does not exist. No description found!");
-  } else {
-    const data = JSON.parse(body)[0].description;
-    console.log(data);
-    console.log(typeof(data));
-  }
+const fetchBreedDescription = function(breed, callback) {
+  const givenURL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breed;
+  request.get(givenURL, (err, response, body) => {
+    if (err) {
+      return callback(err)
+    }
+    if (JSON.parse(body)[0] === undefined) {
+      return callback(null,"Breed does not exist")
+    } else {
+      callback(null, JSON.parse(body)[0].description.trim())
+    }
+  });
+}
 
-});
+
+
+module.exports = {fetchBreedDescription};
