5c5597733a72452a64ed3277f2588105f5747a45
Aiman Azmie <aimanaaw@gmail.com>
2019-09-26 18:49:07 +0000

    Commiting updates and corrections

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,11 +2,10 @@ const request = require('request');
 const userInputURL = process.argv.slice(2);
 const givenURL = 'https://api.thecatapi.com/v1/breeds/search?q=' + userInputURL;
 
-request(givenURL, (err, response, body) => {
+request.get(givenURL, (err, response, body) => {
   if (err) {
     console.log("Breed not found: ", err);
-  };
-    
+  }
   if (JSON.parse(body)[0] === undefined) {
     console.log("Error: The breed does not exist. No description found!");
   } else {
